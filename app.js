ba740ccec60b20b2939d4cc6c9335aa033a78758
Moses Ventura <moses2308ventura@gmail.com>
2024-01-25 12:54:19 -0800

    initialized project

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+    Card 
+        member : should have a suit (symbol)
+        member : should have a rank (number value)
+        function : compare two cards
+    Deck 
+        member : array of cards[]
+        function : shuffle the deck
+        function : split deck into two
+    Player 
+        member : a deck
+        member : a score
+
+    who should be in charge of splitting decks
+
+    HOW THE GAME WORKS
+        (deck) shuffle main deck
+        split deck into two
+        assign half decks to players
+        top cards are revealed
+        cards are compared
+        
+        if cards are different
+            player with greater rank wins
+        if cards are the same
+            neither player wins
+*/
