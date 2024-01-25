3d1716855901644471de069d2c39a38d8ef9c2d1
Moses Ventura <moses2308ventura@gmail.com>
2024-01-25 13:15:23 -0800

    card class created and completed compare method

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+class Card {
+  constructor(suit, rank) {
+    this.suit = suit; //string
+    this.rank = rank; //numeric
+  }
+  compareTo(card) {
+    //returns true if argument is lower
+    if (this.rank > card.rank) {
+      return true;
+    }
+    return false;
+  }
+}
+
 /*
     Card 
         member : should have a suit (symbol)
         member : should have a rank (number value)
-        function : compare two cards
-    Deck 
+        function : compare two cards by rank then suit [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A ] suit doesnt matter 13 ranks [clubs, diamonds, hearts, spades]
+    Deck
         member : array of cards[]
         function : shuffle the deck
         function : split deck into two
